ab3612bb32c2234809b1578e64824edc44b9f5f6
Jonas Wanke <contact@jonas-wanke.com>
2024-11-09 09:42:22 +0200

    Add navigation cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,38 @@
-import { Card } from "@/components/ui/card";
-import { H1 } from "@/components/ui/typography";
+import { Card } from '@/components/ui/card';
+import { H2 } from '@/components/ui/typography';
+import Link from 'next/link';
 
 export default function Home() {
-  return (
-    <Card className="w-full">
-      <H1 className="m-4">HK Foods</H1>
-      <H1 className="m-4">HK Sustain</H1>
-    </Card>
+    return (
+        <div className="flex items-stretch gap-8">
+            <NavigationCard
+                title="Dashboard"
+                href="/dashboard"
+            />
+            <NavigationCard
+                title="Preproduction Tablet App"
+                href="/preproduction-tablet"
+            />
+            <NavigationCard
+                title="Manager App"
+                href="/manager-app"
+            />
+        </div>
+    );
+}
 
-
-
-  );
+interface NavigationCardProps {
+    title: string;
+    href: string;
+}
+function NavigationCard({ title, href }: NavigationCardProps) {
+    return (
+        <Link
+            className="flex-1"
+            href={href}>
+            <Card className="h-full flex flex-col justify-center content-center p-8">
+                <H2 className="m-0 text-center border-none">{title}</H2>
+            </Card>
+        </Link>
+    );
 }
