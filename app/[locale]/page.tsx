86cf59d817bdd9a0afd2b2d5599cb68c3ffeb6f2
Simon Brebeck <simonbrebeck@yahoo.de>
2024-11-10 00:38:05 +0200

    Make manager-app not clickable

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -135,31 +135,40 @@ interface NavigationCardProps {
 }
 
 function NavigationCard({ title, href, description, disabled = false }: NavigationCardProps) {
+    const card = (
+        <Card
+            className={`${disabled ? 'bg-gray-100' : ''} h-full flex flex-col justify-center content-center p-6`}>
+            <H4 className={`${disabled ? 'opacity-50' : ''} m-0 text-center border-none`}>{title}</H4>
+        </Card>
+    );
+
     return (
-        <HoverCard>
+        <HoverCard
+            openDelay={500}
+            closeDelay={300}>
             <HoverCardTrigger asChild>
-                <Link
-                    className="w-60 h-32"
-                    href={href}>
-                    <Card
-                        className={`${disabled ? 'bg-gray-100' : ''} h-full flex flex-col justify-center content-center p-6`}>
-                        <H4 className={`${disabled ? 'opacity-50' : ''} m-0 text-center border-none`}>
-                            {title}
-                        </H4>
-                    </Card>
-                </Link>
+                {disabled ? (
+                    <div className="w-60 h-32 cursor-not-allowed">{card}</div>
+                ) : (
+                    <Link
+                        className="w-60 h-32"
+                        href={href}>
+                        {card}
+                    </Link>
+                )}
             </HoverCardTrigger>
             <HoverCardContent className={'w-80'}>
-                {!disabled ? (
-                    <div className={`flex justify-between space-x-4 `}>
-                        <div className="space-y-1">
-                            <h4 className={`text-sm font-semibold `}>{title}</h4>
-                            <p className={`text-sm whitespace-pre-wrap`}>{description}</p>
-                        </div>
+                <div className={`flex flex-col justify-between`}>
+                    <div className="space-y-1">
+                        <h4 className={`text-sm font-semibold `}>{title}</h4>
+                        <p className={`text-sm whitespace-pre-wrap`}>{description}</p>
                     </div>
-                ) : (
-                    <div>You need to select a &#39;manager&#39; role to navigate to the manager app!</div>
-                )}
+                    {disabled && (
+                        <div className={'text-red-700 font-bold mt-3'}>
+                            You need to select a &#39;manager&#39; role to navigate to the manager app!
+                        </div>
+                    )}
+                </div>
             </HoverCardContent>
         </HoverCard>
     );
