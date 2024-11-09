a076fe1d0fb26ef4df76427f4444c7358ce81e53
Jonas Wanke <contact@wanke.dev>
2024-11-09 23:32:46 +0000

    Change dashboard language every 5 seconds

--- app/[locale]/dashboard/[area]/page.tsx
+++ app/[locale]/dashboard/[area]/page.tsx
@@ -3,13 +3,27 @@
 import { Dashboard } from '@/components/dashboard/dashboard';
 import { Separator } from '@/components/ui/separator';
 import { Leaderboard } from '@/components/dashboard/leaderboard';
+import { languages } from '@/components/language-selector/language-selector';
 import { Area } from '@/lib/models';
 import { useGlobalState } from '@/hooks/useModels';
 import { useParams } from 'next/navigation';
 
 export default function DashboardPage() {
     const { area } = useParams<{ area: Area }>();
-    const messages = useGlobalState().dashboardMessages[area];
+    const messages = useGlobalState().dashboardMessages[area
+    const locale = useLocale();
+
+    usePeriodic(
+        () => 
+        router.replace(
+            // @ts-expect-error -- TypeScript will validate that only known `params`
+            // are used in combination with a given `pathname`. Since the two will
+            // always match for the current route, we can skip runtime checks.
+            { pathname, params },
+            { locale: languages[(languages.findIndex((it) => it.code == locale) + 1) % languages.length] },
+        ),
+        5000
+    );
 
     return (
         <div className={'relative flex pb-12 flex-row gap-2 w-full text-lg'}>
@@ -22,4 +36,11 @@ export default function DashboardPage() {
             <footer className={'absolute bottom-1 -translate-x-1/2 left-1/2 text-center'}>{messages}</footer>
         </div>
     );
+}
+
+function usePeriodic(callback: () => void, durationMillis: number) {
+   useEffect(() => {
+    const interval = setInterval(callback, durationMillis)
+    return () => clearInterval(interval)
+   }, [durationMillis])
 }
