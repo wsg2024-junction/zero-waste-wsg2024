5b481877578be8d3df257b31b375e876e2c4655d
Jonas Wanke <contact@jonas-wanke.com>
2024-11-10 03:20:12 +0200

    Fix compiler errors

--- app/[locale]/dashboard/[area]/page.tsx
+++ app/[locale]/dashboard/[area]/page.tsx
@@ -1,28 +1,38 @@
 'use client';
 
 import { Dashboard } from '@/components/dashboard/dashboard';
-import { Separator } from '@/components/ui/separator';
 import { Leaderboard } from '@/components/dashboard/leaderboard';
 import { languages } from '@/components/language-selector/language-selector';
-import { Area } from '@/lib/models';
+import { Separator } from '@/components/ui/separator';
 import { useGlobalState } from '@/hooks/useModels';
+import { usePathname, useRouter } from '@/i18n/routing';
+import { Area } from '@/lib/models';
+import { useLocale } from 'next-intl';
 import { useParams } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function DashboardPage() {
     const { area } = useParams<{ area: Area }>();
-    const messages = useGlobalState().dashboardMessages[area
+    const messages = useGlobalState().dashboardMessages[area];
+
+    const router = useRouter();
+    const pathname = usePathname();
+    const params = useParams();
     const locale = useLocale();
 
     usePeriodic(
-        () => 
-        router.replace(
-            // @ts-expect-error -- TypeScript will validate that only known `params`
-            // are used in combination with a given `pathname`. Since the two will
-            // always match for the current route, we can skip runtime checks.
-            { pathname, params },
-            { locale: languages[(languages.findIndex((it) => it.code == locale) + 1) % languages.length] },
-        ),
-        5000
+        () =>
+            router.replace(
+                // @ts-expect-error -- TypeScript will validate that only known `params`
+                // are used in combination with a given `pathname`. Since the two will
+                // always match for the current route, we can skip runtime checks.
+                { pathname, params },
+                {
+                    locale: languages[(languages.findIndex((it) => it.code == locale) + 1) % languages.length]
+                        .code,
+                },
+            ),
+        3000,
     );
 
     return (
@@ -39,8 +49,8 @@ export default function DashboardPage() {
 }
 
 function usePeriodic(callback: () => void, durationMillis: number) {
-   useEffect(() => {
-    const interval = setInterval(callback, durationMillis)
-    return () => clearInterval(interval)
-   }, [durationMillis])
+    useEffect(() => {
+        const interval = setInterval(callback, durationMillis);
+        return () => clearInterval(interval);
+    }, [callback, durationMillis]);
 }
