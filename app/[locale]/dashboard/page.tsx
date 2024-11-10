34c3e4146597f073fb0c6e17819fc2c16021e642
Simon Brebeck <simonbrebeck@yahoo.de>
2024-11-10 02:16:18 +0200

    Move emergency warning to dashboard

--- app/[locale]/dashboard/page.tsx
+++ app/[locale]/dashboard/page.tsx
@@ -3,39 +3,15 @@
 import { Dashboard } from '@/components/dashboard/dashboard';
 import { Separator } from '@/components/ui/separator';
 import { Leaderboard } from '@/components/dashboard/Leaderboard';
-import { useEffect, useState } from 'react';
-import { Area, AreaStatus } from '@/lib/models';
+import { useState } from 'react';
+import { Area } from '@/lib/models';
 import { useGlobalState } from '@/hooks/useModels';
-import { TriangleAlert, TriangleAlertIcon } from 'lucide-react';
 
-function hasState(states: Record<Area, AreaStatus>, state: AreaStatus) {
-    return Object.values(states).includes(state);
-}
 export default function DashboardPage() {
     const [area, setArea] = useState<Area>('preproduction');
     const messages = useGlobalState().dashboardMessages[area];
-    const globalState = useGlobalState();
-    const [worstState, setWorstState] = useState(AreaStatus.OK);
-
-    useEffect(() => {
-        if (hasState(globalState.status, AreaStatus.EMERGENCY)) setWorstState(AreaStatus.EMERGENCY);
-        else if (hasState(globalState.status, AreaStatus.STOPPED)) setWorstState(AreaStatus.STOPPED);
-        else if (hasState(globalState.status, AreaStatus.SLOWED)) setWorstState(AreaStatus.SLOWED);
-        else setWorstState(AreaStatus.OK);
-    }, [globalState]);
-
     return (
         <div className={'relative flex pb-12 flex-row gap-2 w-full text-lg'}>
-            {worstState === AreaStatus.EMERGENCY && (
-                <div
-                    className={
-                        'fixed top-0 left-0 w-full h-full z-20 bg-red-500 text-white flex flex-col justify-center items-center'
-                    }>
-                    <TriangleAlertIcon size={64} />
-                    <span className={'font-bold text-3xl'}>EMERGENCY</span>
-                    <span className={'font-bold text-md'}>Please seek your nearest manager!</span>
-                </div>
-            )}
             <Dashboard />
             <Separator
                 orientation={'vertical'}
