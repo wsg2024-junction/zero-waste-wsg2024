44cc8da316be6e19da93c49ccf10868655c4ef12
Simon Brebeck <simonbrebeck@yahoo.de>
2024-11-10 08:29:25 +0200

    Fix overflow status selection

--- components/manager-status-selection/StatusSelection.tsx
+++ components/manager-status-selection/StatusSelection.tsx
@@ -57,29 +57,29 @@ function SingleAreaStatusSelection({
             <div className={'flex flex-row gap-2'}>
                 <Button
                     onClick={() => onChangeStatus('OK')}
-                    variant={currentStatus === 'OK' ? 'ghost' : 'outline'}
-                    className={`w-20 border-green-600 ${currentStatus === 'OK' ? 'hover:bg-green-600 hover:text-white bg-green-500 text-white' : 'text-green-600'}`}>
+                    variant="ghost"
+                    className={`border-green-600 ${currentStatus === 'OK' ? 'hover:bg-green-600 hover:text-white bg-green-500 text-white' : 'text-green-600'}`}>
                     <CheckIcon />
                     <span>{t('managerApp.areaStatus.ok')}</span>
                 </Button>
                 <Button
                     onClick={() => onChangeStatus('SLOWED')}
-                    variant={currentStatus === 'SLOWED' ? 'ghost' : 'outline'}
-                    className={`w-24 border-orange-600  ${currentStatus === 'SLOWED' ? 'hover:bg-orange-600 hover:text-white bg-orange-500 text-white' : 'text-orange-600'}`}>
+                    variant="ghost"
+                    className={`border-orange-600  ${currentStatus === 'SLOWED' ? 'hover:bg-orange-600 hover:text-white bg-orange-500 text-white' : 'text-orange-600'}`}>
                     <TurtleIcon />
                     <span>{t('managerApp.areaStatus.slowed')}</span>
                 </Button>
                 <Button
                     onClick={() => onChangeStatus('STOPPED')}
-                    variant={currentStatus === 'STOPPED' ? 'ghost' : 'outline'}
-                    className={`w-24 border-red-600 ${currentStatus === 'STOPPED' ? 'hover:bg-red-600 hover:text-white bg-red-500 text-white' : 'text-red-600'}`}>
+                    variant="ghost"
+                    className={`border-red-600 ${currentStatus === 'STOPPED' ? 'hover:bg-red-600 hover:text-white bg-red-500 text-white' : 'text-red-600'}`}>
                     <PauseIcon />
                     <span>{t('managerApp.areaStatus.stopped')}</span>
                 </Button>
                 <Button
                     onClick={() => onChangeStatus('EMERGENCY')}
-                    variant={currentStatus === 'EMERGENCY' ? 'destructive' : 'outline'}
-                    className={`w-36 border-red-600 ${currentStatus === 'EMERGENCY' ? '' : 'text-red-600'}`}>
+                    variant={currentStatus === 'EMERGENCY' ? 'destructive' : 'ghost'}
+                    className={`border-red-600 ${currentStatus === 'EMERGENCY' ? '' : 'text-red-600'}`}>
                     <TriangleAlertIcon />
                     <span>{t('managerApp.areaStatus.emergency')}</span>
                 </Button>
